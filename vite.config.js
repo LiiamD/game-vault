13460212d36c025fec82aec1e506af51fd71ec60
Liamine D. <liamine14@hotmail.fr>
2025-10-06 12:53:15 +0000

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/game-vault/',
-  plugins: [react(), tailwindcss()]
-  
+  plugins: [react(), tailwindcss()],
+  base: '/game-vault/'
 })
